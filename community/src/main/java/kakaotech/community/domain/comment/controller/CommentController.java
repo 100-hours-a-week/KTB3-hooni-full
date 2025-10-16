2e00d0b682223488c4ec3c1e0b92d54781068c31
hoooonshub <yhn515@naver.com>
2025-10-16 23:34:45 +0900

    fix: 댓글 수정 메서드 오류 수정

--- community/src/main/java/kakaotech/community/domain/comment/controller/CommentController.java
+++ community/src/main/java/kakaotech/community/domain/comment/controller/CommentController.java
@@ -9,9 +9,9 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -29,7 +29,7 @@ public class CommentController {
         return ResponseEntity.ok(commentService.create(userId, postId, request.content()));
     }
 
-    @PutMapping("/{commentId}")
+    @PatchMapping("/{commentId}")
     public ResponseEntity<CommentResponse.Key> update(@Authenticated Long userId,
                                                       @PathVariable Long postId,
                                                       @PathVariable Long commentId,
