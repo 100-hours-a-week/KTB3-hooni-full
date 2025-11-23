ab5dbfba7ac4b3652f4a597bc2734bcc06c64931
hoooonshub <yhn515@naver.com>
2025-11-23 14:20:53 +0900

    fix: localhost 5500(js)에 대한 cors 설정 추가

--- community/src/main/java/kakaotech/community/global/config/WebMvcConfig.java
+++ community/src/main/java/kakaotech/community/global/config/WebMvcConfig.java
@@ -4,6 +4,7 @@ import kakaotech.community.global.auth.resolver.AuthenticatedUserArgumentResolve
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
@@ -16,5 +17,16 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
         resolvers.add(authenticatedUserArgumentResolver);
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:5500")
+                .allowedMethods("GET","POST","PUT","PATCH","DELETE","OPTIONS")
+                .allowedHeaders("*")
+                .exposedHeaders("Authorization")
+                .allowCredentials(true)
+                .maxAge(3600);
     }
 }
