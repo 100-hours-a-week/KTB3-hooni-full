1f412091c2d072688c474e9a8b15019830c370cb
hoooonshub <yhn515@naver.com>
2025-11-24 08:00:51 +0900

    feat: 비밀번호 Encoder 추가 및 인코딩 기능 추가

--- community/src/main/java/kakaotech/community/domain/user/service/AuthService.java
+++ community/src/main/java/kakaotech/community/domain/user/service/AuthService.java
@@ -3,6 +3,7 @@ package kakaotech.community.domain.user.service;
 import kakaotech.community.domain.user.User;
 import kakaotech.community.domain.user.port.Token;
 import kakaotech.community.domain.user.port.TokenGenerator;
+import kakaotech.community.domain.user.port.encode.Encoder;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -12,13 +13,14 @@ import org.springframework.transaction.annotation.Transactional;
 public class AuthService {
     private final UserService userService;
 
+    private final Encoder encoder;
     private final TokenGenerator tokenGenerator;
 
     // FIXME. 이메일 틀린 시 UserException 발생 -> AuthException으로 변경 필요
     @Transactional(readOnly = true)
     public Token login(String email, String password) {
         User user = userService.findByEmail(email);
-        user.validateLoginable(password);
+        user.validateLoginable(encoder, password);
 
         return tokenGenerator.login(user.getId());
     }
---
+++ community/src/main/java/kakaotech/community/global/security/encode/DefaultEncoder.java
@@ -0,0 +1,22 @@
+package kakaotech.community.global.security.encode;
+
+import kakaotech.community.domain.user.port.encode.Encoder;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class DefaultEncoder implements Encoder {
+    private final PasswordEncoder passwordEncoder;
+
+    @Override
+    public String encode(String rawPassword) {
+        return passwordEncoder.encode(rawPassword);
+    }
+
+    @Override
+    public boolean matches(String rawPassword, String encodedPassword) {
+        return passwordEncoder.matches(rawPassword, encodedPassword);
+    }
+}
