293f43e5ef0d11d5d13bbec75ac9a496faded7f4
hoooonshub <yhn515@naver.com>
2025-11-28 14:32:39 +0900

    feat: MethodArgumentNotValidException 핸들러 추가

--- community/src/main/java/kakaotech/community/global/exception/advice/GlobalExceptionHandler.java
+++ community/src/main/java/kakaotech/community/global/exception/advice/GlobalExceptionHandler.java
@@ -1,9 +1,13 @@
 package kakaotech.community.global.exception.advice;
 
 import kakaotech.community.global.exception.BaseException;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.Map;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
@@ -11,5 +15,10 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(BaseException.class)
     public ResponseEntity<?> handleBaseException(BaseException exception) {
         return ResponseEntity.status(exception.getHttpStatus()).body(exception.getMessage());
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<?> handleMethodArgumentNotValidException(MethodArgumentNotValidException exception) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(Map.of("message", "잘못된 값으로 요청됨"));
     }
 }
